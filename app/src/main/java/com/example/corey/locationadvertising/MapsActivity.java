2d66d28e862d6948f096ee39b19389f74d6d7013
Corey Gallagher <l00115921@student.lyit.ie>
2018-02-06 16:37:54 +0000

    Maps Edited

--- app/src/main/java/com/example/corey/locationadvertising/MapsActivity.java
+++ app/src/main/java/com/example/corey/locationadvertising/MapsActivity.java
@@ -36,14 +36,14 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
 
                 if (ContextCompat.checkSelfPermission( this, Manifest.permission.ACCESS_FINE_LOCATION ) == PackageManager.PERMISSION_GRANTED) {
-                    {
+
                     locationManager.requestLocationUpdates( LocationManager.GPS_PROVIDER, 0, 0, locationListener );
                 }
             }
         }
     }
 
-}
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -59,15 +59,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     }
 
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
+    //Manipulates the map once available.
     @Override
     public void onMapReady(GoogleMap googleMap) {
          mMap = googleMap;
